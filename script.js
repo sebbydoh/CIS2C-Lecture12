0af3ea93985d7cdcd0a2e377c7e85e4e1956a450
Encinia <sebastienencinia@my.whccd.edu>
2020-02-19 21:17:38 -0800

    Finished

--- script.js
+++ script.js
@@ -3,10 +3,31 @@
 let myGrades = [100, 100, 90, 73, 78, 94, 86];
 let myAverage = 0;
 for (let i = 0; i <= myGrades.length - 1; i++) {
-myAverage += myGrades[i];
+  myAverage += myGrades[i];
 }
 myAverage = myAverage / myGrades.length;
 console.log("My grade average is " + myAverage);
 
+//Part 2
 
+class InputError extends Error {}
 
+function myNumber(a) {
+  let answer = a;
+
+  if (isNaN(a) || a < 0 || a > 100) {
+    throw new InputError("Invalid input error.");
+  }
+
+  return answer;
+}
+
+try {
+  console.log(myNumber(101));
+} catch (error) {
+  if (error instanceof InputError) {
+    console.log(error.message);
+  } else {
+    console.log("Generic error message.");
+  }
+}

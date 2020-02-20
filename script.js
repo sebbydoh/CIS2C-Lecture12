2ba7e87ff985f68c358779ecbcd6a08acb9f94eb
Encinia <sebastienencinia@my.whccd.edu>
2020-02-19 20:38:57 -0800

    Debugged average grade code

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-myGrades = [100, 100, 90, 73, 78, 94, 86];
-myAverage = 0;
-for (let i = 1; i <= myGrades.length; i++) {
+"use strict";
+
+let myGrades = [100, 100, 90, 73, 78, 94, 86];
+let myAverage = 0;
+for (let i = 0; i <= myGrades.length - 1; i++) {
 myAverage += myGrades[i];
 }
 myAverage = myAverage / myGrades.length;
 console.log("My grade average is " + myAverage);
+
+
+
